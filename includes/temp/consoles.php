5d806060dfba33d615c0c2f71a324267af1d9e19
4tutoralcom <4tutoralcom@gmail.com>
2014-09-11 08:43:44 -0500

    improved trade shop.

--- includes/temp/consoles.php
+++ includes/temp/consoles.php
@@ -1,78 +1,87 @@
 <?php
 include('simple_html_dom.php');
-$row = 1;
-$console_name = isset($_GET['console-name']) ? (string)$_GET['console-name'] : "";
-$product_name = isset($_GET['product-name']) ? strtolower((string)$_GET['product-name']) : "";
-$page = isset($_GET['p']) ? intval((string)$_GET['p']) : "";
-$CellOrder=array();
-$JSON_Array=array();
-$Console_pic=array();
-$curentConsoleName="";
-function getArt($id,$console, $GameTitle,&$Console_pic,&$curentConsoleName, &$grabFormWeb=1){	
-	$found=false;
-	$idUrlPair=array();
-	$imgUrl="";
-	$STOP=false;
-	//echo(($curentConsoleName==$console)?count($Console_pic).",":"n");
-	if (($handle = fopen("pic/".$console."_pic.csv", "r")) !== FALSE && $curentConsoleName==$console && count($Console_pic)==0) {		
-		while (($data = fgetcsv($handle, 1000, ",")) !== False) {
-			if(isset($data[1]))
-				array_push($Console_pic, array($data[0]=>$data[1]));
-			else
-				echo($data[0]);
-		}
-		fclose($handle);	
-	}
-	
-	if(isset($Console_pic[$id])){
-	//echo("y");
-		$imgUrl=$Console_pic[$id];
-		$found=($imgUrl!=="");
-	}else{
-	$curentConsoleName=$console;
-		if (($handle = fopen("pic/".$console."_pic.csv", "r")) !== FALSE) {		
-			while (($data = fgetcsv($handle, 1000, ",")) !== False) {
-				$item=array();
-				$num = count($data);
-				if($num==2 && $data[0]==$id){
-					$imgUrl=$data[1];
-					break;
-				}
-			}
-			fclose($handle);
-			$found=($imgUrl!=="");
-		}
-	}
-	if(!$found && $grabFormWeb>0){
-		$grabFormWeb--;
-		$cons=str_replace(" ","-",$console);
-		$title=str_replace(" ","-",$GameTitle);
-		$title=str_replace("/","",$title);
-		$title=str_replace(".","",$title);
-		$title=str_replace("?","",$title);
-		$title=str_replace("!","",$title);
-		$title=str_replace(",","",$title);
-		$title=str_replace(":","",$title);
-		$title=str_replace("#","%23",$title);
-		$url = "http://videogames.pricecharting.com/game/$cons/$title";
-		//echo($id."|".$url."\n");
-		//$html = file_get_html("http://nimishprabhu.com");
-		$dom = str_get_html(file_get_contents($url));
-		if(is_object($dom)){
-			$img = $dom->find('#product_details > div > img');
-			if(is_array($img) && count ($img)>0 && is_object($img[0])){
-			
-				$imgUrl=$img[0]->getAttribute("src");
-				
-				$fp = fopen("pic/".$console."_pic.csv", 'a');
-				fputcsv($fp, array($id,$imgUrl));
-				fclose($fp);
-			}
-		}else{
-		die($url);
-		}
-	}
-	return $imgUrl;
+$row               = 1;
+$gameID              = isset($_GET['id']) ? intval((string) $_GET['id']) : -1;
+$console_name      = isset($_GET['console-name']) ? (string) $_GET['console-name'] : "";
+$product_name      = isset($_GET['product-name']) ? strtolower((string) $_GET['product-name']) : "";
+$page              = isset($_GET['p']) ? intval((string) $_GET['p']) : "";
+$CellOrder         = array();
+$JSON_Array        = array();
+$Console_pic       = array();
+$curentConsoleName = "";
+
+function getArt($id, $console, $GameTitle, &$Console_pic, &$curentConsoleName, &$grabFormWeb = 1)
+{
+    $found     = false;
+    $idUrlPair = array();
+    $imgUrl    = "";
+    $STOP      = false;
+    //echo(($curentConsoleName==$console)?count($Console_pic).",":"n");
+    if (($handle = fopen("pic/" . $console . "_pic.csv", "r")) !== FALSE && $curentConsoleName == $console && count($Console_pic) == 0) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== False) {
+            if (isset($data[1]))
+                array_push($Console_pic, array(
+                    $data[0] => $data[1]
+                ));
+            else
+                echo ($data[0]);
+        }
+        fclose($handle);
+    }
+    
+    if (isset($Console_pic[$id])) {
+        //echo("y");
+        $imgUrl = $Console_pic[$id];
+        $found  = ($imgUrl !== "");
+    } else {
+        $curentConsoleName = $console;
+        if (($handle = fopen("pic/" . $console . "_pic.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== False) {
+                $item = array();
+                $num  = count($data);
+                if ($num == 2 && $data[0] == $id) {
+                    $imgUrl = $data[1];
+                    break;
+                }
+            }
+            fclose($handle);
+            $found = ($imgUrl !== "");
+        }
+    }
+    if (!$found && $grabFormWeb > 0) {
+        $grabFormWeb--;
+        $cons  = str_replace(" ", "-", $console);
+        $title = str_replace(" ", "-", $GameTitle);
+        $title = str_replace("/", "", $title);
+        $title = str_replace(".", "", $title);
+        $title = str_replace("?", "", $title);
+        $title = str_replace("!", "", $title);
+        $title = str_replace(",", "", $title);
+        $title = str_replace(":", "", $title);
+        $title = str_replace("#", "%23", $title);
+		
+        $url   = "http://videogames.pricecharting.com/game/$cons/$title";
+        //echo($id."|".$url."\n");
+        //$html = file_get_html("http://nimishprabhu.com");
+        $dom   = str_get_html(file_get_contents($url));
+        if (is_object($dom)) {
+            $img = $dom->find('#product_details > div > img');
+            if (is_array($img) && count($img) > 0 && is_object($img[0])) {
+                
+                $imgUrl = $img[0]->getAttribute("src");
+                
+                $fp = fopen("pic/" . $console . "_pic.csv", 'a');
+                fputcsv($fp, array(
+                    $id,
+                    $imgUrl
+                ));
+                fclose($fp);
+            }
+        } else {
+            die($url);
+        }
+    }
+    return $imgUrl;
 }
 set_time_limit(600);
 header("Content-Type: application/json");
@@ -81,54 +90,62 @@ if (($handle = fopen("price-guide.csv", "r")) !== FALSE) {
         $num = count($data);
         //echo "<p> $num fields in line $row: <br /></p>\n";
         $row++;
-        for ($c=0; $c < $num; $c++) {
+        for ($c = 0; $c < $num; $c++) {
             //echo $data[$c] . "<br />1\n";
-			array_push($CellOrder,$data[$c]);
+            array_push($CellOrder, $data[$c]);
         }
     }
-	$r=200;
-	$page-=1;
-	$page*=24;
-	$itemcounter=0;
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-	//while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-
-		
-			$item=array();
-			$num = count($data);
-			//echo "<p> $num fields in line $row: <br /></p>\n";
-			$row++;
-			for ($c=0; $c < $num; $c++) {
-				$item[$CellOrder[$c]]=$data[$c];
-				//echo $CellOrder[$c].":".$data[$c] . "<br />\n";
-			}
-			
-			if($console_name==="" || $item["console-name"]==$console_name){
-				if($product_name!==""){
-					$nin=strpos(strtolower($item["product-name"]),strtolower($product_name));
-					$nin=(false===$nin)?0:$nin+1;
-				}
-				if($product_name==="" || $nin>0){
-				if($itemcounter>=$page && $itemcounter < ($page+24)){
-						if(!isset($item["image"])){
-							$curentConsoleName=$item["console-name"];
-							$item["image"]=getArt($item["id"],$item["console-name"],$item["product-name"],$Console_pic,$curentConsoleName,$r);
-						}
-						
-						array_push($JSON_Array,$item);
-						
-						//echo($item["id"].",".$item["product-name"].",".$item["console-name"].",".$itemcounter.">=".$page."\n");
-						
-					}
-					$itemcounter++;
-				}
-			}
-		
-		
-    }
+    $r = 200;
+    $page = ($page-1)*24;
+    $itemcounter = 0;
 	
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        //while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        
+        
+        $item = array();
+        $num  = count($data);
+        //echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c = 0; $c < $num; $c++) {
+            $item[$CellOrder[$c]] = $data[$c];
+            //echo $CellOrder[$c].":".$data[$c] . "<br />\n";
+        }
+        
+        if ($console_name === "" || $item["console-name"] == $console_name) {
+            if ($product_name !== "" && $gameID==-1) {
+                $nin = strpos(strtolower($item["product-name"]), strtolower($product_name));
+                $nin = (false === $nin) ? 0 : $nin + 1;
+            }else if($product_name === "" && $gameID==$item["id"]){
+                    if (!isset($item["image"])) {
+                        $curentConsoleName = $item["console-name"];
+                        $item["image"]     = getArt($item["id"], $item["console-name"], $item["product-name"], $Console_pic, $curentConsoleName, $r);
+                    }
+					array_push($JSON_Array, $item);
+			}
+			if ($product_name === "" || $nin > 0) {
+                if ($itemcounter >= $page && $itemcounter < ($page + 24)) {
+                    if (!isset($item["image"])) {
+                        $curentConsoleName = $item["console-name"];
+                        $item["image"]     = getArt($item["id"], $item["console-name"], $item["product-name"], $Console_pic, $curentConsoleName, $r);
+                    }
+                    
+                    array_push($JSON_Array, $item);
+                    
+                    //echo($item["id"].",".$item["product-name"].",".$item["console-name"].",".$itemcounter.">=".$page."\n");
+                    
+                }
+                $itemcounter++;
+            }
+        }
+        
+        
+    }
+    
     fclose($handle);
 }
-array_unshift($JSON_Array,["pages"=>(($itemcounter-$itemcounter%24)/24)+(($itemcounter%24==0)?0:1)]);
+if($gameID===-1)
+	array_unshift($JSON_Array,["pages"=>(($itemcounter-$itemcounter%24)/24)+(($itemcounter%24==0)?0:1)]);
+
 echo json_encode($JSON_Array);
 ?>
--- trade.php
+++ trade.php
@@ -2,161 +2,163 @@
 require 'includes\part\header.php';
 require_once('includes/template.php');
 
-$id = isset($_GET['id']) ? (string)$_GET['id'] : -1;
+$id = isset($_GET['id']) ? (string) $_GET['id'] : -1;
 
-$GameTitle="";
-$Platform="";
-$front_image="";
-$back_image="";
+$GameTitle = "";
+$Platform  = "";
+$image     = "";
 
-$uid=0;
+$uid = 0;
 
-if($id!==-1){
-	$file = file_get_contents("http://localhost/includes/game.php?id=".$id);
-	$jsonIterator = new RecursiveIteratorIterator(
-		new RecursiveArrayIterator(json_decode($file, TRUE)),
-		RecursiveIteratorIterator::SELF_FIRST);
-	foreach ($jsonIterator as $key => $value) {
-	if($key=="GameTitle")
-		$GameTitle=$value;
-	elseif($key=="Platform")
-		$Platform=$value;
-	elseif($key=="images"){
-		if(isset($value["boxart"])){
-			if(isset($value["boxart"]["front"]))
-				$front_image=$value["boxart"]["front"];
-			if(isset($value["boxart"]["back"]))
-				$back_image=$value["boxart"]["back"];
-		}
-	}elseif($key=="error")
-		if($value=="id"){
-			echo("error");
-		}
-	}
-}else{
-$file = file_get_contents('includes/temp/consoles.json');
-$jsonIterator = new RecursiveIteratorIterator(
-	new RecursiveArrayIterator(json_decode($file, TRUE)),
-	RecursiveIteratorIterator::SELF_FIRST);
-	
-$tempShop = new Template('includes/temp/SearchItem.tpl');
-$typeTempplate='<option data-toggle="loadGamesFromJson" frame-target="#Second" group-target="[name]">[name]</option>';
-$tempType = new Template($typeTempplate);
-
-$groupTemplate='
+if ($id !== -1) {
+    $file         = file_get_contents("http://localhost/includes/temp/consoles.php?id=" . $id);
+    $jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($file, TRUE)), RecursiveIteratorIterator::SELF_FIRST);
+    foreach ($jsonIterator as $key => $value) {
+        if ($key == "product-name")
+            $GameTitle = $value;
+        elseif ($key == "console-name")
+            $Platform = $value;
+        elseif ($key == "image") {
+            $image = $value;
+        } elseif ($key == "error")
+            if ($value == "id") {
+                echo ("error");
+            }
+    }
+} else {
+    $file         = file_get_contents('includes/temp/consoles.json');
+    $jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($file, TRUE)), RecursiveIteratorIterator::SELF_FIRST);
+    
+    $tempShop      = new Template('includes/temp/SearchItem.tpl');
+    $typeTempplate = '<option data-toggle="loadGamesFromJson" frame-target="#Second" group-target="[name]">[name]</option>';
+    $tempType      = new Template($typeTempplate);
+    
+    $groupTemplate       = '
 <div class="group" id="[Type]">
-	<select class="selectpicker">
-		<option data-toggle="setSearch" data-target="#products" data-value="[Type]">All</option>
-		[optionGroup]
-	</select>
+    <select class="selectpicker">
+        <option data-toggle="setSearch" data-target="#products" data-value="[Type]">All</option>
+        [optionGroup]
+    </select>
 </div>';
-$optionGroupTempalte='<optgroup label="[companyName]" data-subtext="" data-icon="icon-ok">[items]</optgroup>';
-$optionTemplate='<option data-toggle="setSearch" data-target="#products" data-value="[name]">[name]</option>';
+    $optionGroupTempalte = '<optgroup label="[companyName]" data-subtext="" data-icon="icon-ok">[items]</optgroup>';
+    $optionTemplate      = '<option data-toggle="setSearch" data-target="#products" data-value="[name]">[name]</option>';
 }
 ?>
-	<div class="container bg-white main-content">
-		<?php if($id!==-1):?>
-		<div class="coverArtContainer  col-xs-12 col-md-6">
-				<img class="nentendo" src="<?php echo 'http://localhost/test.php?src='.urlencode($front_image).'&type='.urlencode($Platform)?>"></img>
-			
-			<div class="nentendo" id="title"><?php echo $GameTitle ?></div>
-			<div class="nentendo" id="platform"><?php echo $Platform ?></div>
-			
-		</div>
-		<div class="Pricing col-xs-12 col-md-6">
+  <div class="container bg-white main-content">
+        <?php
+if ($id !== -1):
+?>
+      <div class="coverArtContainer  col-sm-5 col-md-5">
+                <img class="nentendo" src="<?php
+    echo $image;
+?>"></img>
+            
+            <div class="nentendo" id="title"><?php
+    echo $GameTitle;
+?></div>
+            <div class="nentendo" id="platform"><?php
+    echo $Platform;
+?></div>
+            
+        </div>
+        <div class="Pricing col-xs-12 col-md-6">
 
-		</div>
-		<?php else:?>
-		<div class="frame" id="Shop">
-			<div class="group current" id="Search">
-				<h1 id="tittle">Select Game</h1>
-				<div class="row">
-				<div class="col-xs-12">
-					<label for="first_sortBY">Search By : </label>
-				</div>
-					<div class="col-md-6 col-xs-6 frame">
-						<div class="group current">
-							<select class="selectpicker">
-								<option selected data-toggle="fade setSearch" data-target="#products" data-value="" frame-target="#Second" group-target="">None</option>
-								<optgroup label="Type" data-subtext="" data-icon="icon-ok">
-									<?php
-										$tempSet=false;
-											$values= array();
-											$types = array();
-											$producttitle="";
-											$company="Games";
-											foreach ($jsonIterator as $key => $val) {
-												if(!is_array($val)) {
-													if($key=="name"){
-														$tempType->setTag($key,$val);
-														$tempSet=true;
-													}
-												}elseif($tempSet){
-													echo $tempType->output();
-													$tempType = new Template($typeTempplate);
-													$tempSet=false;
-												}
-												
-											}
-											if($tempSet)
-												echo $tempType->output();
-										?>
-								</optgroup>
-								<optgroup label="Attributes" data-subtext="" data-icon="icon-ok">
-									<option data-toggle="fade" frame-target="#Second" group-target="#Text">Text</option>
-								</optgroup>
-							</select>
-						</div>
-					</div>
-					<div class="col-md-6 col-xs-6 frame" id="Second">
-						<?php
-							foreach ($values as $type=>$TypeArray) {
-								$tempGroup = new Template($groupTemplate);
-								$tempGroup->setTag("Type",$type);
-								$optionGroup="";
-								foreach ($TypeArray as $companyName=>$itemsArray) {
-									$tempOptionGroup = new Template($optionGroupTempalte);
-									$tempOptionGroup->setTag("companyName",$companyName);
-									$items="";
-									foreach ($itemsArray as &$name) {
-										$tempOption = new Template($optionTemplate);
-										$tempOption->setTag("name",$name);
-										
-										$items.=$tempOption->output();
-									}
-									$tempOptionGroup->setTag("items",$items);
-									$optionGroup.=$tempOptionGroup->output();
-								}
-								
-								$tempGroup->setTag("optionGroup",$optionGroup);
-								echo $tempGroup->output();
-							}
-						?>
-						<div id="Text">
-								<div class="bs-float-label">
-									<label for="search" class="float-label">Search Name</label>
-									<div class="input-group">
-										<span class="input-group-addon"><i class="fa fa-search" id="search button"></i></span>
-										<input type="text" class="form-control float-input" name="SearchName" id="SearchName" placeholder="Search Name" autocomplete="off">
-									</div>
-								</div>
-						</div>
-					</div>
-				</div>
-				<ul class="pagination">
+        </div>
+        <?php
+else:
+?>
+      <div class="frame" id="Shop">
+            <div class="group current" id="Search">
+                <h1 id="tittle">Select Game</h1>
+                <div class="row">
+                <div class="col-xs-12">
+                    <label for="first_sortBY">Search By : </label>
+                </div>
+                    <div class="col-md-6 col-xs-6 frame">
+                        <div class="group current">
+                            <select class="selectpicker">
+                                <option selected data-toggle="fade setSearch" data-target="#products" data-value="" frame-target="#Second" group-target="">None</option>
+                                <optgroup label="Type" data-subtext="" data-icon="icon-ok">
+                                    <?php
+    $tempSet      = false;
+    $values       = array();
+    $types        = array();
+    $producttitle = "";
+    $company      = "Games";
+    foreach ($jsonIterator as $key => $val) {
+        if (!is_array($val)) {
+            if ($key == "name") {
+                $tempType->setTag($key, $val);
+                $tempSet = true;
+            }
+        } elseif ($tempSet) {
+            echo $tempType->output();
+            $tempType = new Template($typeTempplate);
+            $tempSet  = false;
+        }
+        
+    }
+    if ($tempSet)
+        echo $tempType->output();
+?>
+                              </optgroup>
+                                <optgroup label="Attributes" data-subtext="" data-icon="icon-ok">
+                                    <option data-toggle="fade" frame-target="#Second" group-target="#Text">Text</option>
+                                </optgroup>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xs-6 frame" id="Second">
+                        <?php
+    foreach ($values as $type => $TypeArray) {
+        $tempGroup = new Template($groupTemplate);
+        $tempGroup->setTag("Type", $type);
+        $optionGroup = "";
+        foreach ($TypeArray as $companyName => $itemsArray) {
+            $tempOptionGroup = new Template($optionGroupTempalte);
+            $tempOptionGroup->setTag("companyName", $companyName);
+            $items = "";
+            foreach ($itemsArray as &$name) {
+                $tempOption = new Template($optionTemplate);
+                $tempOption->setTag("name", $name);
+                
+                $items .= $tempOption->output();
+            }
+            $tempOptionGroup->setTag("items", $items);
+            $optionGroup .= $tempOptionGroup->output();
+        }
+        
+        $tempGroup->setTag("optionGroup", $optionGroup);
+        echo $tempGroup->output();
+    }
+?>
+                      <div id="Text">
+                                <div class="bs-float-label">
+                                    <label for="search" class="float-label">Search Name</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="fa fa-search" id="search button"></i></span>
+                                        <input type="text" class="form-control float-input" name="SearchName" id="SearchName" placeholder="Search Name" autocomplete="off">
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="pagination">
 
-				</ul>
-				<div class="gamesList" id="products" class="row list-group">
+                </ul>
+                <div class="gamesList" id="products" class="row list-group">
 
-				</div>
-				<ul class="pagination">
+                </div>
+                <ul class="pagination">
 
-				</ul>
-			</div>
-		</div>
-		<?php endif?>
-	</div>
-	
+                </ul>
+            </div>
+        </div>
+        <?php
+endif;
+?>
+  </div>
+    
 <?php
 require 'includes\part\footer.php';
 ?>
